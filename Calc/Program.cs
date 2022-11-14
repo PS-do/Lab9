22ccb054f86a228c5c46a8b0a2d63b1eef35ec33
Сергей Плюхин <psg.pgs@mail.ru>
2022-11-14 21:16:49 +0300

    Добавьте файлы проекта.

---
+++ Calc/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, rez;
+            int k;
+            Console.WriteLine("\"Калькулятор\" запущен");
+            try
+            {
+                Console.WriteLine("Введите первое число:");
+                x1 = Convert.ToDouble(Console.ReadLine());
+                try
+                {
+                    Console.WriteLine("Введите второе число:");
+                    x2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите код операции:\n" +
+                        "\t1\t-\tСложение\n" +
+                        "\t2\t-\tВычитание\n" +
+                        "\t3\t-\tпроизведение\n" +
+                        "\t4\t-\tчастное\n");
+                    Console.Write("Ваш выбор:");
+                    try
+                    {
+                        k = Convert.ToInt32(Console.ReadLine());
+                        switch (k)
+                        {
+                            //case 0:
+                            //    {
+                            //        Console.WriteLine();
+                            //        break;
+                            //    }
+                            case 1:
+                                {
+                                    Console.WriteLine(x1 + x2);
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    Console.WriteLine(x1 - x2);
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    Console.WriteLine(x1 * x2);
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    try 
+                                    { Console.WriteLine(x1 / x2); }// !!Подскажите пожалуйста, почему здесь не выскакивает сообщение об ошибке?
+                                    catch 
+                                    {
+                                        Console.WriteLine("Деление на 0!");
+                                    }
+                                    break;
+                                }
+                            default:
+                                { 
+                                    Console.WriteLine("Введена не верная команда");
+                                    break;
+                                }
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp_R_l/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_R_l
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double l = Convert.ToDouble(Console.ReadLine());
+                double r = l / (2 * Math.PI);
+                Console.WriteLine(r);
+            }
+            catch(Exception ex)
+            { Console.WriteLine(ex.Message); }
+            Console.ReadKey();
+        }
+    }
+}
